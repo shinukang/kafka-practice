110200527e50faa79ef0813b00174009202f6d17
shinukang <shinu9643@gmail.com>
2026-03-05 12:46:16 +0900

    [feat] 컨슈머 Key-Value를 String-String에서 Long-Json으로 변경

---
+++ consumer/src/main/java/org/example/consumer/model/MessageDto.java
@@ -0,0 +1,13 @@
+package org.example.consumer.model;
+
+import lombok.Getter;
+
+public class MessageDto {
+
+    @Getter
+    public static class Abcd {
+        private Long idx;
+        private String abcd01;
+        private String abcd02;
+    }
+}
